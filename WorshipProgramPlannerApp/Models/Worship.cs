4dcb81438970bf12011fbd59b637c1814b684634
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2025-02-01 15:03:10 -0500

    Added Model classes - Worship and WorshipProgram

---
+++ WorshipProgramPlannerApp/Models/Worship.cs
@@ -0,0 +1,10 @@
+﻿namespace WorshipProgramPlannerApp.Models
+{
+    public class Worship
+    {
+        public int WorshipId { get; set; }
+        public DateTime WorshipDate { get; set; }
+        public string WorshipName { get; set; }
+        public ICollection<WorshipProgram> WorshipPrograms { get; set; } = new List<WorshipProgram>();
+    }
+}
---
+++ WorshipProgramPlannerApp/Models/WorshipProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace WorshipProgramPlannerApp.Models
+{
+    public class WorshipProgram
+    {
+        public int WorshipProgramId { get; set; }
+        public string PerformerName { get; set; }
+        public string PoetryName { get; set; }
+        public string SongName { get; set; }
+        public string Comment { get; set; }
+
+        // Foreign Key
+        public int WorshipId { get; set; }
+        public Worship Worship { get; set; }
+    }
+}
