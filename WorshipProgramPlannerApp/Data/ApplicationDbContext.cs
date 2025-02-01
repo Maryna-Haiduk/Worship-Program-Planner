223ecc9e084fdcc1ca660dccf24c516afddef302
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2025-02-01 15:18:01 -0500

    Created the Database Context

---
+++ WorshipProgramPlannerApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorshipProgramPlannerApp.Models;
+
+
+namespace WorshipProgramPlannerApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Worship> Worships { get; set; }
+        public DbSet<WorshipProgram> WorshipPrograms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
