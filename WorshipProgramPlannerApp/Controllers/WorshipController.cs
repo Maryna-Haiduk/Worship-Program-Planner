ff51e9d81e893bf23a6c7ad2815ec342ec3f8678
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2025-02-01 21:23:39 -0500

    Added Controllers

---
+++ WorshipProgramPlannerApp/Controllers/WorshipController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorshipProgramPlannerApp.Models;
+using WorshipProgramPlannerApp.Repositories;
+
+namespace WorshipProgramPlannerApp.Controllers
+{
+    public class WorshipController : Controller
+    {
+        private readonly IWorshipRepository _worshipRepository;
+
+        public WorshipController(IWorshipRepository worshipRepository)
+        {
+            _worshipRepository = worshipRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var worships = _worshipRepository.GetAll();
+            return View(worships);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var worship = _worshipRepository.GetById(id);
+            if (worship == null) return NotFound();
+            return View(worship);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Worship worship)
+        {
+            if (ModelState.IsValid)
+            {
+                _worshipRepository.Add(worship);
+                _worshipRepository.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(worship);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var worship = _worshipRepository.GetById(id);
+            if (worship == null) return NotFound();
+            return View(worship);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Worship worship)
+        {
+            if (id != worship.WorshipId) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _worshipRepository.Update(worship);
+                _worshipRepository.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(worship);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var worship = _worshipRepository.GetById(id);
+            if (worship == null) return NotFound();
+            return View(worship);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _worshipRepository.Delete(id);
+            _worshipRepository.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WorshipProgramPlannerApp/Controllers/WorshipProgramController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorshipProgramPlannerApp.Repositories;
+using WorshipProgramPlannerApp.Models;
+using WorshipProgramPlannerApp.Repositories;
+
+namespace WorshipProgramPlanner.Controllers
+{
+    public class WorshipProgramController : Controller
+    {
+        private readonly IWorshipProgramRepository _worshipProgramRepository;
+
+        public WorshipProgramController(IWorshipProgramRepository worshipProgramRepository)
+        {
+            _worshipProgramRepository = worshipProgramRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var worshipPrograms = _worshipProgramRepository.GetAll();
+            return View(worshipPrograms);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var worshipProgram = _worshipProgramRepository.GetById(id);
+            if (worshipProgram == null) return NotFound();
+            return View(worshipProgram);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(WorshipProgram worshipProgram)
+        {
+            if (ModelState.IsValid)
+            {
+                _worshipProgramRepository.Add(worshipProgram);
+                _worshipProgramRepository.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(worshipProgram);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var worshipProgram = _worshipProgramRepository.GetById(id);
+            if (worshipProgram == null) return NotFound();
+            return View(worshipProgram);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, WorshipProgram worshipProgram)
+        {
+            if (id != worshipProgram.WorshipProgramId) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _worshipProgramRepository.Update(worshipProgram);
+                _worshipProgramRepository.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(worshipProgram);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var worshipProgram = _worshipProgramRepository.GetById(id);
+            if (worshipProgram == null) return NotFound();
+            return View(worshipProgram);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _worshipProgramRepository.Delete(id);
+            _worshipProgramRepository.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
